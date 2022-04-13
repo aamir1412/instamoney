7e8f196a127fb21576ffc68e44b23d79d3561f69
Prateek Bhuwania <prateekchinu3011@gmail.com>
2022-04-13 01:59:14 -0400

    offer loan working

--- ballot-contract/contracts/InstaMoney.sol
+++ ballot-contract/contracts/InstaMoney.sol
@@ -2,12 +2,14 @@ pragma solidity >=0.4.22 <=0.6.0;
 
 contract InstaMoney {
 
-    address broker;
-    uint net_funds;
-    int user_id_counter;
+    address payable public broker;
+    uint public net_funds;
+    uint public user_id_counter;
+    uint public offering_id_counter;
+    uint public loan_id_counter;
 
     struct  User {
-        int id;
+        uint id;
         string name;
         string identification;
     }
@@ -18,18 +20,19 @@ contract InstaMoney {
         uint term;
         uint rate;
         address lender;
-        uint ts;        //timestamp
+        bool is_active;
     }
 
     struct Loan {
         uint id;
-        uint offeringId;
+        uint offering_id;
         address borrower;
         uint status;    // 1 = Active, 2 = PaidOff
-        uint ts;
+        uint activated_at;      //timestamp
     }
 
     mapping(address => User) public users;
+    mapping(address => uint) private balances;
     Loan[] public loans;
     Offering[] public offerings;
 
@@ -40,14 +43,22 @@ contract InstaMoney {
     }
 
     ///term is in days
-    function offerLoan(string memory lender_name, string memory identification) public
-                      //  uint term, uint interest_rate) public payable 
-                        {
+    function offerLoan(string memory lender_name, string memory identification,
+                        uint term, uint interest_rate) public payable {
+        require(msg.value != 0, "Poor you. Please offer some money at least");
         if (users[msg.sender].id == 0) {
             User memory user = User({id: user_id_counter++, name: lender_name, identification: identification});
             users[msg.sender] = user;
         }
 
+        Offering memory offer = Offering({id: offering_id_counter++, amount: msg.value, term: term,
+         rate: interest_rate, lender: msg.sender, is_active: true});
+
+        offerings.push(offer);
+
+        balances[msg.sender] += msg.value;
+        net_funds += msg.value;
+        broker.transfer(msg.value);
     }
 
     function getTotalProcessed() view public returns(uint, uint){
@@ -66,7 +77,7 @@ contract InstaMoney {
 
     function cancelOffer(uint offer_id) public returns (bool){
         for (uint j = 0; j < loans.length ; j += 1) {
-            if(loans[j].offeringId == offer_id){
+            if(loans[j].offering_id == offer_id){
                 //cannot be cancelled since already executed as a loan
                 return false;
             }
@@ -96,6 +107,8 @@ contract InstaMoney {
         broker = msg.sender;
         net_funds = 0;
         user_id_counter = 1;
+        offering_id_counter = 1;
+        loan_id_counter = 1;
     }
 
 }
