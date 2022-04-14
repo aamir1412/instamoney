f142651e52f3a5af082a6e14947b2fbcb38019c1
Prateek Bhuwania <prateekchinu3011@gmail.com>
2022-04-14 16:29:40 -0400

    cleaning up some things

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+//var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var InstaMoney = artifacts.require("InstaMoney");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  //deployer.deploy(SimpleStorage);
   deployer.deploy(InstaMoney);
 };
